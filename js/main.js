c74a48706c85d7af7168e1e1d86bb7a43744045b
szymtur <szymtur@gmail.com>
2018-01-22 22:48:54 +0100

    drop-menu-hiding

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    scrolled();
+    hideDropMenu();
+});
+
+$(window).scroll(function() {
     scrolled();
 });
 
-$(window).scroll(function(){
-    scrolled();
-});
-
-function scrolled(){
+function scrolled() {
     var navHeight = $('#main-menu').outerHeight();
     var scrolled = $(window).scrollTop();
-    
-    if(scrolled > navHeight){
+
+    if (scrolled > navHeight) {
         $('#main-menu').addClass('scrolled');
     } else {
         $('#main-menu').removeClass('scrolled');
     }
+}
+
+function hideDropMenu() {
+    $('.menu-item').click(function () {
+        $('#drop-menu').prop('checked', false);
+    });
 }
