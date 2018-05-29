f2068c0e0dc6badcedaf9293ec74be985c9034ec
szymtur <szymtur@gmail.com>
2018-05-29 14:17:34 +0200

    smoothscroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     scrolled();
     hideDropMenu();
+    smoothScroll();
 });
 
 $(window).scroll(function() {
@@ -21,5 +22,21 @@ function scrolled() {
 function hideDropMenu() {
     $('.menu-item').click(function () {
         $('#drop-menu').prop('checked', false);
+    });
+}
+
+function smoothScroll(){
+    $("a[href*=\\#]").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        } 
     });
 }
